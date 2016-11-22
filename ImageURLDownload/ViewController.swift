b0d38ab50cc473c4a1e74e7c3c847e7667eb2208
Andrew Barber <edwardbar423@gmail.com>
2016-11-22 13:30:56 -0500

    Initial Commit

---
+++ ImageURLDownload/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ImageURLDownload
+//
+//  Created by Andrew Barber on 11/22/16.
+//  Copyright © 2016 Invictus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
